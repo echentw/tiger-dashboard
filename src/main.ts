a1f00300484ba6ac12a3fcaacc5c6e89fcabc33b
Eric Chen <ericchen098@gmail.com>
2019-01-28 23:43:48 -0800

    reskin

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,18 @@ function main() {
   setInterval(updateCurrentTime, 1 * 1000); // every second
   setInterval(updateETAs, 10 * 1000); // every 10 seconds
   setInterval(updateWeather, 2 * 60 * 60 * 1000); // every 2 hours
-
-  blinkArrow();
 }
 
 async function updateETAs() {
   const data: NLineData = await Model.getNLineETAs();
 
-  const predictions = data.NOwl.predictions;
+  const container = $('.n-judah-etas-container');
 
-  for (let i = 0; i < Math.min(3, predictions.length); ++i) {
+  container.empty();
+
+  const predictions = data.NJudah.predictions;
+
+  for (let i = 0; i < Math.min(5, predictions.length); ++i) {
     const eta = predictions[i];
 
     const absoluteTimeString: string = new Date(eta.absoluteTimeSeconds).toLocaleTimeString('en-US');
@@ -30,56 +32,46 @@ async function updateETAs() {
 
     const displayString = `${absoluteTimeString} (${minutes} min ${seconds} sec)`;
     if (i === 0) {
-      $('#predictions').append(`<div id="prediction"><b>${displayString}</b></div>`);
+      container.append(`<div class="n-judah-eta text"><b>${displayString}</b></div>`);
     } else {
-      $('#predictions').append(`<div id="prediction">${displayString}</div>`);
-    }
-  }
-  if (predictions.length < 3) {
-    for (var i = 0; i < 3 - predictions.length; ++i) {
-      $('#predictions').append('<div id="prediction" style="color: white">you cant see me</div>');
+      container.append(`<div class="n-judah-eta text">${displayString}</div>`);
     }
   }
 }
 
 function updateCurrentTime() {
   var date = new Date();
-  $('#current-time').empty();
-  $('#current-time').append(`<div>${Model.getCurrentTime()}</div>`);
+  $('.time-container').empty();
+  $('.time-container').append(`${Model.getCurrentTime()}`);
 }
 
 function updateWeather() {
   $.ajax({
     url: 'http://api.openweathermap.org/data/2.5/weather?id=5391959&APPID=2aff0e5cbbfc2c468be071ac2aa778f4',
     success: function(data: any) {
-      $('#today-weather').empty();
+      const dateContainer = $('.date-container');
+      const forecastContainer = $('.forecast-container');
+      const temperatureContainer = $('.temperature-container');
 
-      var description = data['weather'][0]['description'];
-      var tempKelvin = Number(data['main']['temp']);
-      var tempFahrenheit = Math.round(tempKelvin * 9.0 / 5.0 - 459.67);
-      var tempCelsius = Math.round(tempKelvin - 273.15);
+      dateContainer.empty();
+      forecastContainer.empty();
+      temperatureContainer.empty();
 
-      var date = new Date();
-      var dayOfWeek = [
+      const description = data['weather'][0]['description'];
+      const tempKelvin = Number(data['main']['temp']);
+      const tempFahrenheit = Math.round(tempKelvin * 9.0 / 5.0 - 459.67);
+      const tempCelsius = Math.round(tempKelvin - 273.15);
+
+      const date = new Date();
+      const dayOfWeek = [
         'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
       ][ date.getDay() ];
 
-      $('#today-weather').append('<div>' + dayOfWeek + ', ' + date.toLocaleDateString() + '</div>');
-      $('#today-weather').append('<div>' + description + '</div>');
-      $('#today-weather').append(
-        '<div>' + String(tempFahrenheit) + '&deg;F / ' + String(tempCelsius) + '&deg;C</div>'
-      );
+      dateContainer.append(`${dayOfWeek}, ${date.toLocaleDateString()}`);
+      forecastContainer.append(description);
+      temperatureContainer.append(`${tempFahrenheit} &deg;F / ${tempCelsius} &deg;C`);
     },
   });
-}
-
-function blinkArrow() {
-  setInterval(function() {
-    $('i').hide();
-    setTimeout(function() {
-      $('i').show();
-    }, 800);
-  }, 2000);
 }
 
 main();
